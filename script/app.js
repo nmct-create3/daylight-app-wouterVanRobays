b7a4c778c0ed506457265e41aa64c2e4922a67df
WouterVanRobays <wouter.van.robays@student.howest.be>
2019-10-18 12:34:17 +0200

    einde les

--- script/app.js
+++ script/app.js
@@ -1,4 +1,5 @@
 // _ = helper functions
+let domsun;
 function _parseMillisecondsIntoReadableTime(timestamp) {
 	//Get hours from milliseconds
 	const date = new Date(timestamp * 1000);
@@ -14,21 +15,36 @@ function _parseMillisecondsIntoReadableTime(timestamp) {
 }
 
 // 5 TODO: maak updateSun functie
-
+let updateSun = function(time) {
+	domSun.setAttribute('data-time', time);
+};
 // 4 Zet de zon op de juiste plaats en zorg ervoor dat dit iedere minuut gebeurt.
 let placeSunAndStartMoving = (totalMinutes, sunrise) => {
 	// In de functie moeten we eerst wat zaken ophalen en berekenen.
 	// Haal het DOM element van onze zon op en van onze aantal minuten resterend deze dag.
 	const domTimeLeft = document.querySelector('.js-time-left');
-	const domSun = document.querySelector('.js-sun');
+	domSun = document.querySelector('.js-sun');
 	// Bepaal het aantal minuten dat de zon al op is.
 	const today = new Date();
-	const minutes = (today.getHours()*60) + today.getMinutes();
+	const minutes = today.getHours() * 60 + today.getMinutes();
+	console.log('total: ' + totalMinutes);
+	console.log('minutes: ' + minutes);
 	// Nu zetten we de zon op de initiële goede positie ( met de functie updateSun ). Bereken hiervoor hoeveel procent er van de totale zon-tijd al voorbij is.
-	console.log(totalMinutes);
+	if (minutes % 60 < 10) {
+		//console.log("minutes: " + Math.round(minutes/60)+ ":0" + minutes%60);
+		updateSun(Math.floor(minutes / 60) + ':0' + (minutes % 60));
+	} else {
+		updateSun(Math.floor(minutes / 60) + ':' + (minutes % 60));
+		//console.log("minutes: " + Math.round(minutes/60)+ ":" + minutes%60);
+	}
 	// We voegen ook de 'is-loaded' class toe aan de body-tag.
+	document.querySelector('.js-body').classList.add('is-loaded');
 	// Vergeet niet om het resterende aantal minuten in te vullen.
+
 	// Nu maken we een functie die de zon elke minuut zal updaten
+	setInterval(function() {
+		getAPI(50.8027841, 3.2097454);
+	}, 60 * 1000);
 	// Bekijk of de zon niet nog onder of reeds onder is
 	// Anders kunnen we huidige waarden evalueren en de zon updaten via de updateSun functie.
 	// PS.: vergeet weer niet om het resterend aantal minuten te updaten en verhoog het aantal verstreken minuten.
@@ -44,27 +60,34 @@ let showResult = queryResponse => {
 	// Toon ook de juiste tijd voor de opkomst van de zon en de zonsondergang.
 	const domsunrise = document.querySelector('.js-sunrise');
 	const domsunset = document.querySelector('.js-sunset');
-	domsunrise.innerHTML = _parseMillisecondsIntoReadableTime(queryResponse.city.sunrise);
-	domsunset.innerHTML = _parseMillisecondsIntoReadableTime(queryResponse.city.sunset);
+	domsunrise.innerHTML = _parseMillisecondsIntoReadableTime(
+		queryResponse.city.sunrise
+	);
+	domsunset.innerHTML = _parseMillisecondsIntoReadableTime(
+		queryResponse.city.sunset
+	);
 	// Hier gaan we een functie oproepen die de zon een bepaalde positie kan geven en dit kan updaten.
-	const sunset = new Date(queryResponse.city.sunset*1000);
-	const sunrise = (queryResponse.city.sunrise/1000);
+	const sunset = new Date(queryResponse.city.sunset * 1000);
+	const sunrise = queryResponse.city.sunrise / 1000;
 	console.log(sunset);
-	placeSunAndStartMoving(((queryResponse.city.sunset - queryResponse.city.sunrise)/1000)/60, queryResponse.city.sunrise);
+	placeSunAndStartMoving(
+		((queryResponse.city.sunset - queryResponse.city.sunrise) / 1000) * 60,
+		queryResponse.city.sunrise
+	);
 };
 
 // 2 Aan de hand van een longitude en latitude gaan we de yahoo wheater API ophalen.
 let getAPI = (lat, lon) => {
 	// Eerst bouwen we onze url op
-	let url =  `http://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=06d0d2396e1cb1381580708a2e94e258&units=metric&lang=nl&cnt=1`
+	let url = `http://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=ab342d92a6fa8d23069b4ade0788c0e3&units=metric&lang=nl&cnt=1`;
 	// Met de fetch API proberen we de data op te halen.
-	fetch(url).then(function(response)
-	{
-		return response.json();
-	}).then(function(data)
-	{
-		showResult(data);
-	});
+	fetch(url)
+		.then(function(response) {
+			return response.json();
+		})
+		.then(function(data) {
+			showResult(data);
+		});
 	// Als dat gelukt is, gaan we naar onze showResult functie.
 };
 
